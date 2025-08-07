26c8d1c91e8fbb7ad5465e64f790f2e4e90b7ffa
Gabriel Cardozo <gabriel_w_cardozo@estudante.sesisenai.org.br>
2025-08-07 00:47:18 +0000

    Aula_02

---
+++ Semana_2/meus_exercicios_flask/Jinja/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    usuario = 'João Cana Brava'
+    return render_template('index.html', nome_usuario = usuario)
+
+@app.route('/perfil/<nome>')
+def perfil(nome):
+    logado = True
+    usuario = nome
+    return render_template('perfil.html', logado = logado, nome_usuario = usuario)
+
+@app.route('/lista_produtos')
+def lista_produtos():
+    produtos = {'cadeira', 'sofa', 'cama'}
+    return render_template('produtos.html', lista = produtos)
+    
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
